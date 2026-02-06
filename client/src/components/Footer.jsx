93adc199f6c0bfc0b3394710d8d85b365f61e1a5
Yadri Amzal <justyamzal@gmail.com>
2026-02-06 11:11:47 +0700

    adding structure of App with Navbar,Footer, etc

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
-const MovieDetails = () => {
+function Footer() {
   return (
     <div>
-      
+        <h1>Footer</h1>
     </div>
   )
 }
 
-export default MovieDetails
+export default Footer
